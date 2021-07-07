0e6fa7234f128575e852a33c9f16bc6be050af63
Tudor Pescaru <tudorp00@gmail.com>
2021-07-07 18:31:27 +0300

    Updates movies page

--- yts_movies/lib/src/presentation/movie_page.dart
+++ yts_movies/lib/src/presentation/movie_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:yts_movies/src/models/movie.dart';
 
 class MoviePage extends StatefulWidget {
@@ -22,85 +23,78 @@ class _MoviePageState extends State<MoviePage> {
         backgroundColor: Colors.green,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width * 1.33,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                    widget._movie.largeCoverImage,
-                  ),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          widget._movie.title,
-                          style: Theme.of(context).textTheme.headline4,
-                        ),
-                        const SizedBox(
-                          height: 4,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              widget._movie.year == 0 ? 'Not available!' : '${widget._movie.year}',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: Colors.white60,
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 16,
-                            ),
-                            Text(
-                              widget._movie.runtime == 0 ? 'Not available!' : '${widget._movie.runtime} minutes',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: Colors.white60,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width * 1.33,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      widget._movie.largeCoverImage,
                     ),
+                    fit: BoxFit.fill,
                   ),
-                  Row(
-                    children: <Widget>[
-                      Text(
-                        widget._movie.rating == 0 ? 'Not available!' : '${widget._movie.rating / 2} / 5 ',
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      const Icon(
-                        Icons.star,
-                        color: Colors.amberAccent,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(5.0),
-              child: Text(
-                widget._movie.summary,
-                style: const TextStyle(
-                  fontSize: 14,
-                  letterSpacing: 1.4,
                 ),
               ),
-            ),
-          ],
+              Container(
+                padding: const EdgeInsets.all(2.5),
+                child: Text(
+                  widget._movie.title,
+                  style: const TextStyle(
+                    fontSize: 32,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(2.5),
+                child: Text(
+                  widget._movie.year == 0 ? 'Not available!' : '${widget._movie.year}',
+                  style: const TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(2.5),
+                child: Text(
+                  widget._movie.runtime == 0 ? 'Not available!' : '${widget._movie.runtime} minutes',
+                  style: const TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(2.5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      widget._movie.rating == 0 ? 'Not available!' : '${widget._movie.rating / 2} / 5 ',
+                      style: const TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    const Icon(
+                      Icons.star,
+                      color: Colors.amberAccent,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(2.5),
+                child: Text(
+                  widget._movie.summary,
+                  style: const TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
